822cf9e0b24dec46a9379689b1e6ade2386c3e3b
Replit user <>
2023-05-01 03:04:44 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -49,45 +49,47 @@ int is_valid(Node* n){
   for(i=0;i<9;i++)
   {
     valido = calloc(sizeof(int),10);
-    for(j=0;j<9;j++)
-    {           
-      if(n->sudo[i][j] !=0)
-      {
-        if(valido[n->sudo[i][j]] == 1)
-          return 0;
-        else if (n->sudo[j][i] !=0)
+      for(j=0;j<9;j++)
+      {           
+        if(n->sudo[i][j] !=0)
+        {
+          if(valido[n->sudo[i][j]] == 1)
+            return 0;
+          else
+            valido[n->sudo[i][j]] = 1;
+        }
+      }
+    free(valido);
+    valido = calloc(sizeof(int),10);
+      for(j=0;j<9;j++)
+      {           
+        if(n->sudo[j][i] !=0)
         {
           if(valido[n->sudo[j][i]] == 1 )
             return 0;
-          else valido[n->sudo[j][i]] = 1;
+          else
+            valido[n->sudo[j][i]] = 1;
         }
+      }
+    free(valido);
+    
+  }
+
+  for(int k=0 ; k<9 ; k++)
+  {
+    for(p=0;p<9;p++){
+      i=3*(k/3) + (p/3) ;
+      j=3*(k%3) + (p%3) ;
+      valido = calloc(sizeof(int),10);
+      if(n->sudo[i][j] !=0)
+      {
+        if(valido[n->sudo[i][j]] == 1 )
+          return 0;
         else
           valido[n->sudo[i][j]] = 1;
-      }
-    }
-    free(valido);
+      }  
   }
-
-
-
-  for (i = 0; i < 9; i += 3) {
-    for (j = 0; j < 9; j += 3) {
-      valido = calloc(sizeof(int),10);
-      for (p = 0; p < 9; p++) {
-        int row = i + p / 3;
-        int col = j + p % 3;
-        if (n->sudo[row][col] != 0) {
-          if (valido[n->sudo[row][col]] == 1) {
-            free(valido);
-            return 0;
-          } else {
-            valido[n->sudo[row][col]] = 1;
-          }
-        }
-      }
-      free(valido);
-    }
-  }
+  free(valido);
   return 1;
 }
 
