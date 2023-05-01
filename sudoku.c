960ac93152c410f929341d549617f3716653907b
Replit user <>
2023-05-01 01:45:34 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -54,11 +54,11 @@ int is_valid(Node* n){
            if(valido[j] != 1)
               valido[j] = 1;
             else
-             return 0;
+             return 1;
          }
        } 
   }
-  return 1;
+  return 0;
 }
 
 
