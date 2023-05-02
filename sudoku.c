ba86c3f4b2c72419d8297e543b960d497126be1a
Replit user <>
2023-05-02 06:50:58 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -121,8 +121,6 @@ List* get_adj_nodes(Node* n){
 
 
 int is_final(Node* n){
-  if (n==NULL)
-    return 0;
   for(int i = 0 ; i < 9 ; i++)
     for(int j = 0 ; j < 9 ; j++)
       if(n->sudo[i][j]==0)
@@ -135,7 +133,7 @@ Node* DFS(Node* initial, int* cont){
 
   push(S, initial);
 
-  while(S!=NULL)
+  while(top(S)!=NULL)
   {
     Node *auxEstado = top(S);
     pop(S);
